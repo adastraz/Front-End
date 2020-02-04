88ba8f5b7a0fa44133a1f17293e6bceac486d911
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-02-03 19:48:35 -0500

    completed form for sign in page
    state is updated properly for the user according to user inputs for username/pass

---
+++ droom/src/components/SignIn.js
@@ -0,0 +1,59 @@
+import React, {useState} from "react";
+
+function SignIn() {
+
+
+    const [user, setUser] = useState({
+        username: '',
+        password: '',
+    });
+
+    //handle any changes made to inputs username/password in the form
+    const handleChanges = event => {
+        console.log("User: ", user);
+        setUser({
+            ...user,
+            [event.target.name]: event.target.value
+        });
+    };
+
+
+    //on submit
+    const submitForm = event => {
+        event.preventDefault();
+        console.log("User to submit: ", user);
+        setUser({username:  "", password: ""})
+
+        //react 2 handle posting/etc
+    }
+
+    return (
+        <form onSubmit={submitForm}>
+            <label htmlFor="username">Username: </label>
+            <input 
+                id="username"
+                type="text"
+                name="username"
+                onChange={handleChanges}
+                value={user.username}
+            />
+
+            <label htmlFor="password">Password: </label>
+            <input 
+                id="password"
+                type="password"
+                name="password"
+                onChange={handleChanges}
+                value={user.password}
+            />
+
+            <button type="submit">Sign In</button>
+
+        </form>
+
+    );
+
+
+}
+
+export default SignIn
