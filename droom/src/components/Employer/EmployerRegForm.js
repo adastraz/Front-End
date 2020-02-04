689210d9b93f75e84165e46d62c619b3a336224a
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-02-04 13:33:39 -0500

    completed basics for form for employer register
    completed company name and description inputs for form. will add more later once we know what to add for the other inputs

--- droom/src/components/Employer/EmployerRegForm.js
+++ droom/src/components/Employer/EmployerRegForm.js
@@ -1,11 +1,53 @@
-// this was for testing delete when you want
+import React, {useState} from 'react'
 
-import React from 'react'
+const EmployerRegForm = () => {
 
-const EmployerRegForm = (props) => {
-    console.log('location', props.location)
+
+    const [employerUser, setEmployerUser] = useState({
+        company_name: "",
+        description: ""
+    });
+
+
+    //handle any changes made to inputs in the form
+    const handleChanges = event => {
+        console.log("User: ", employerUser);
+        setEmployerUser({
+            ...employerUser,
+            [event.target.name]: event.target.value
+        });
+    };
+
+
+    //on submit
+    const submitForm = event => {
+        event.preventDefault();
+        setEmployerUser({company_name: "", description: ""});
+        console.log("User to submit: ", employerUser);
+        
+        //react 2 handle posting/etc
+    }
+
     return (
-        <>{props.location.state.username}</>
+        <form onSubmit={submitForm}>
+            <label htmlFor="companyName">Company Name: </label>
+            <input 
+                id="companyName"
+                type="text"
+                name="company_name"
+                onChange={handleChanges}
+                value={employerUser.company_name}
+            />
+            <label htmlFor="description">Company Description: </label>
+            <input 
+                id="description"
+                type="textarea"
+                name="description"
+                onChange={handleChanges}
+                value={employerUser.description}
+            />
+            <button type="submit">Create Account</button>
+        </form>
     )
 }
 
