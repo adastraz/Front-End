9419c5e9694b88ed84df4c62ab4c01f68e52b9eb
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-02-04 15:29:01 -0500

    added basic setup for employee profile page

--- droom/src/components/Employee/EmployeeProfile.js
+++ droom/src/components/Employee/EmployeeProfile.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
-//         "user_id": 1,
-//         "first_name": "Jonathan",
-//         "last_name": "Chen",
-//         "occupation": "Student",
-//         "experience": "Years of Java programming",
-//         "interest": "Coding, cooking, backpacking"
 
 const EmployeeProfile = props => {
     console.log(props.user)
     return (
         <>
-        
+           <div>
+                <img src={props.user.imgUrl}/>
+                <h1>{props.user.name}</h1>
+           </div> 
+           <div>
+                <h2>{props.user.industry}</h2>
+                <h2>{props.user.experience}</h2>
+           </div>
         </>
     )
 }
