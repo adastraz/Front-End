f090404dd6a048fa7b6ffc2419df175f8e4690c9
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-02-05 15:50:50 -0500

    Added swiping functionality to swipe list of companies

--- droom/src/components/Employee/EmployeeList.js
+++ droom/src/components/Employee/EmployeeList.js
@@ -1,22 +1,71 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {fetchCompanyArray} from '../../actions'
 import {connect} from 'react-redux'
 import EmployerCard from '../Employer/EmployerCard'
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators
+  } from 'reactstrap';
+
 
 const EmployeeList = props => {
+
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
+
+    const next = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === props.array.length - 1 ? 0 : activeIndex + 1;
+        setActiveIndex(nextIndex);
+    }
+    
+    const previous = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === 0 ? props.array.length - 1 : activeIndex - 1;
+        setActiveIndex(nextIndex);
+    }
+    
+    const goToIndex = (newIndex) => {
+        if (animating) return;
+        setActiveIndex(newIndex);
+    }
+
+    const slides = props.array.map((item) => {
+        return (
+          <CarouselItem
+            onExiting={() => setAnimating(true)}
+            onExited={() => setAnimating(false)}
+            key={item.src}
+          >
+            <EmployerCard item={item} />
+            
+        
+          </CarouselItem>
+        );
+      });
+
+
     useEffect(() => {
         props.fetchCompanyArray()
     }, [])
 
     console.log(props.array)
-
-    return (
-        <div>
-            {props.array.map(item => {
-                return <EmployerCard item={item} key={item.id}/>
-            })}
-        </div>
+    return(
+        <Carousel
+            interval={false}
+            activeIndex={activeIndex}
+            next={next}
+            previous={previous}
+        >
+        <CarouselIndicators items={props.array} activeIndex={activeIndex} onClickHandler={goToIndex} />
+            {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+        <CarouselControl  direction="next" directionText="Next" onClickHandler={next} />
+    </Carousel>
     )
+    
 }
 
 const mapStateToProps = state => {
