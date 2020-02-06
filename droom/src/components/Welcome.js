fd6046fe84cac6eb5cb44fef03b38605701cf5b1
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-02-06 13:23:32 -0500

    added basic styling for welcome page

--- droom/src/components/Welcome.js
+++ droom/src/components/Welcome.js
@@ -1,14 +1,20 @@
 import React from "react";
 import {Link} from "react-router-dom"
+import {Jumbotron, Card, CardHeader, Button} from 'reactstrap'
 
 function Welcome() {
   return (
     <div className="welcome">
-      <h1>Welcome to Droom!</h1>
-      <nav>
-        <Link to="/signin">Sign in</Link>
-        <Link to="/signup">Sign up</Link>
-      </nav>
+      <Jumbotron className="Jumbotron">
+        <Card body inverse color="info" className='text-center welcomeCard'>
+          <CardHeader tag="h1">Welcome to Droom!</CardHeader>
+          <nav>
+            <Button className="welcomeButton"><Link className="welcomeLinks" to="/signin">Sign in</Link></Button>
+            <Button className="welcomeButton"><Link className="welcomeLinks" to="/signup">Sign up</Link></Button>
+          </nav>
+        </Card>
+ 
+      </Jumbotron>
     </div>
   )
 }
